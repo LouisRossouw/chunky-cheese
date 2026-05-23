ea03b9141af2f23fc2d698dc6efd8cc46b6b9557
Louis <lourou99@gmail.com>
2026-05-23 12:04:00 +0200

    Small import cleanup

--- pi/dot_squad/led_map/__init__.py
+++ pi/dot_squad/led_map/__init__.py
@@ -1,11 +1,14 @@
 from .boot import animation as boot
 from .error import animation as error
+
 from .notify_01 import animation as notify_01
 from .notify_02 import animation as notify_02
+
 from .heartbeat import animation as heartbeat
-from .motion_detected import animation as motion_detected
-from .motion_dim import animation as motion_dim
+
 from .motion_off import animation as motion_off
+from .motion_dim import animation as motion_dim
+from .motion_detected import animation as motion_detected
 
 led_animations = {
     "boot": boot,
