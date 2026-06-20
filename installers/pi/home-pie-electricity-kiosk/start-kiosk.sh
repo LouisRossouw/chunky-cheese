8d9c5ff229b150a6236c774a8f7d2ead37225955
Louis <lourou99@gmail.com>
2026-06-21 00:53:21 +0200

    Fix

--- installers/pi/home-pie-electricity-kiosk/start-kiosk.sh
+++ installers/pi/home-pie-electricity-kiosk/start-kiosk.sh
@@ -14,7 +14,6 @@ chromium \
 --disable-translate \
 --password-store=basic \
 --autoplay-policy=no-user-gesture-required \
---enable-virtual-keyboard \
 --load-extension=/opt/dot_squad_bridge,/opt/nav-nav \
 --user-data-dir=/home/pi/.config/chromium-kiosk \
 "$KIOSK_URL"
