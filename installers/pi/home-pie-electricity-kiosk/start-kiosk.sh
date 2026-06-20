a4a8482343a110b81ef814c196cf566ec2a5ac3f
Louis <lourou99@gmail.com>
2026-06-20 23:21:14 +0200

    Added nav nav to the home-pie installers

--- installers/pi/home-pie-electricity-kiosk/start-kiosk.sh
+++ installers/pi/home-pie-electricity-kiosk/start-kiosk.sh
@@ -14,6 +14,6 @@ chromium \
 --disable-translate \
 --password-store=basic \
 --autoplay-policy=no-user-gesture-required \
---load-extension=/opt/dot_squad_bridge \
+--load-extension=/opt/dot_squad_bridge,/opt/nav-nav \
 --user-data-dir=/home/pi/.config/chromium-kiosk \
 "$KIOSK_URL"
