42b7da5b2bef14760670f9aa96b794fd76b18b38
Louis <lourou99@gmail.com>
2026-07-11 10:48:46 +0200

    Blue heartbeat please

--- pi/moonraker_events/states/idle.py
+++ pi/moonraker_events/states/idle.py
@@ -4,7 +4,7 @@ from animations.heartbeat import Heartbeat
 class IdleState:
 
     def apply(self, leds):
-        leds.segment("heartbeat").animation = Heartbeat(color=(0, 200, 0))
+        leds.segment("heartbeat").animation = Heartbeat(color=(0, 0, 200))
 
         off = Solid((0, 0, 0))
         leds.segment("inside_left").animation = off
